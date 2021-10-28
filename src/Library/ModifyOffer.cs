da0a4ca2cfe0471eedc289a4a077b2b5e729b5e3
facundopereiram <facundopmm@gmail.com>
2021-10-28 16:39:22 -0300

    Se modifican clase Register,AdminUser,EntrepreneurUser,User,Offer,ModifyOffer

--- src/Library/ModifyOffer.cs
+++ src/Library/ModifyOffer.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Collections.Generic;
 namespace ClassLibrary
 {
     /// <summary>
@@ -7,5 +7,26 @@ namespace ClassLibrary
     /// </summary>
     public class ModifyOffer
     {
+         public void Quantity(Offer NewOffer,int NewQuantity )
+        {
+             NewOffer.QuantityMaterial=NewQuantity;
+            
+        }
+        public void Keywords(Offer NewOffer,string NewKeyword )
+        {
+             NewOffer.Keywords=NewKeyword;
+            
+        }
+        public void Disponibility(Offer NewOffer,bool NewDisponibility )
+        {
+             NewOffer.Disponibility=NewDisponibility;
+            
+        }
+        public void Term(Offer NewOffer,int NewExtendTerm )
+        {
+             NewOffer.Term=NewExtendTerm;
+            
+        }
     }
 }
+ 
