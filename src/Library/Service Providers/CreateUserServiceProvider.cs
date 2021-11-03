acf4893e9fd0c1512373e42d79352358a46c1b24
TyMata <69222882+TyMata@users.noreply.github.com>
2021-11-03 14:10:13 -0300

    Se creo CreateUserServiceProvider

--- src/Library/Service Providers/CreateUserServiceProvider.cs
+++ src/Library/Service Providers/CreateUserServiceProvider.cs
@@ -5,16 +5,33 @@ using System.Text;
 namespace ClassLibrary
 {
     /// <summary>
-    /// Esta clase se encarga de crear usuarios y 
+    /// Esta clase se encarga de crear usuarios de empresas y usuarios emprendedores
     /// </summary>
     public class CreateUserServiceProvider
     {
+        /// <summary>
+        /// Crea un usuario empresa
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="company"></param>
         public static void CreateCompanyUser(IMessage input,Company company)
         {
             IRole rol = new CompanyRole(company);
             User usuario = new User(input.Id, rol);
-            
+            CompanyServiceProvider.AddUserToCompany(usuario,  company);
         }
-        
+        /// <summary>
+        /// Crea un usuario emprendedor
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="name"></param>
+        /// <param name="location"></param>
+        /// <param name="headings"></param>
+        /// <param name="habilitations"></param>
+        public static void CreateEntrepreneurUser(IMessage input,string name , Location location, string headings, string habilitations)
+        {
+            IRole rol = new EntrepreneurRole(name , location, headings, habilitations);
+            User usuario = new User(input.Id, rol);
+        }
     }
 }
