5a7ad884728c89f437dfabf33372cc06855500fd
Juan Riccetto <juanmartinre@gmail.com>
2021-11-03 23:45:34 -0300

    Se crea usertest y company role test

---
+++ test/LibraryTests/CompanyRoleTest.cs
@@ -0,0 +1,31 @@
+using ClassLibrary;
+using NUnit.Framework;
+
+namespace Tests
+{
+    /// <summary>
+    /// Prueba de la clase <see cref="Material"/>.
+    /// </summary>
+    [TestFixture]
+    public class CompanyRoleTest
+    {
+       
+        
+         [SetUp]
+        public void Setup()
+        {
+        //this.client = new LocationApiClient();
+        
+
+       
+        }
+        [Test]
+
+        public void CreateMaterialTest()
+        {
+            
+            
+        }
+
+    }
+}
