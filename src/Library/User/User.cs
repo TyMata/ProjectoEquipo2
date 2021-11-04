e9199af94b8bf5dcd70185d690db7c2c0c0a1436
TyMata <69222882+TyMata@users.noreply.github.com>
2021-11-03 23:54:39 -0300

    Se agregan comentarios y README

--- src/Library/User/User.cs
+++ src/Library/User/User.cs
@@ -3,7 +3,11 @@ using System;
 namespace ClassLibrary
 {
     /// <summary>
-    /// Esta clase 
+    /// Esta clase representa un usuario.
+    /// Decidimos crearla de esta manera porque solo tiene la responsabilidad de conocer los datos
+    /// de un usuario asi cumple con el SRP, tambien decidimos que el Role sea un IRole por el LSP,
+    /// asi al momento de crear un usuario se le puede otorgar cualquiera de los 3 roles (AdminRole,
+    /// CompanyRole o EntrepreneurRole) ya que son subtipos de IRole.
     /// </summary>
     public class User 
     {
