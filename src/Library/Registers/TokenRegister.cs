a2559a8c6733f35e98b8ff467225e3707932966c
facundopereiram <facundopmm@gmail.com>
2021-11-10 13:05:02 -0300

    Se arregla Token Register

--- src/Library/Registers/TokenRegister.cs
+++ src/Library/Registers/TokenRegister.cs
@@ -13,6 +13,47 @@ namespace ClassLibrary
         /// Diccionario con nombre de empresas y sus respectivos tokens habilitados
         /// </summary>
         /// <returns></returns>
-        public static Dictionary<Company , List<string>> TokenList = new Dictionary<Company, List<string>>();
+        public  Dictionary<string , Company> TokenList = new Dictionary<string,Company>();
+
+        /// <summary>
+        /// Verifica si el token es valido
+        /// </summary>
+        /// <param name="codigo"></param>
+        /// <returns></returns>
+        public Company IsValidToken(string codigo)
+        {
+            if (!TokenList.ContainsKey(codigo))
+            {
+                throw new Exception();
+            }
+            return TokenList[codigo];
+
+            
+        }
+        //  public static string GenerateToken(string nameCompany)
+        // {
+        //     Random rnd = new Random();
+        //     StringBuilder token = new StringBuilder();
+        //     bool response;
+        //     Company company = Singleton<CompanyRegister>.Instance.GetCompanyByUserId(int id);
+        //     if(response)
+        //     {    for (int i = 0; i < 3; i++)         //Creo un nuevo token
+        //         {
+        //             int num = rnd.Next(10000, 100000);
+        //             token.Append(num.ToString());
+        //             if (i != 2) token.Append("-");
+        //         }
+        //         if (TokenRegister.TokenList.ContainsKey(company))        //Me fijo si ya existe la empresa y de ser asi le añado el Token a la lista
+        //         {
+        //             List<string> listaActualizada;
+        //             TokenRegister.TokenList.TryGetValue(company, out listaActualizada);
+        //             listaActualizada.Add(token.ToString());
+        //             TokenRegister.TokenList[company]= listaActualizada;
+        //             return token.ToString();
+        //         }
+        //     }
+            
+        //     return "No existe la empresa";   //CAMBIAR  POR EXCEPCION
+        // }
     }
 }
