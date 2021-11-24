bfb59eb4d136611a1e7625e725dc3f7d3a686a52
TyMata <69222882+TyMata@users.noreply.github.com>
2021-11-24 10:09:30 -0300

    Se cambian los atributos State y Data a private

--- src/Library/Handlers/UnregistredUserHandlers/UnregisteredCompanyUserHandler.cs
+++ src/Library/Handlers/UnregistredUserHandlers/UnregisteredCompanyUserHandler.cs
@@ -8,17 +8,25 @@ namespace ClassLibrary
     /// </summary>
     public class UnregisteredCompanyUserHandler : AbstractHandler
     {
-        public UnregisteredCompanyUserState State { get; set; }
+        /// <summary>
+        /// Estado para el handler de UnregisteredCompanyUserHandler.
+        /// </summary>
+        /// <value></value>
+        private UnregisteredCompanyUserState state { get; set; }
 
-        public UnregisteredCompanyUserData Data{ get; set; }
+        /// <summary>
+        /// Guarda la información que pasa el usuario por el chat cuando se utiliza el comando UnregisteredCompanyUserHandler.
+        /// </summary>
+        /// <value></value>
+        private UnregisteredCompanyUserData data{ get; set; }
         /// <summary>
         /// Constructor de objetos UnregistredCompanyUserHandler.
         /// </summary>
         public UnregisteredCompanyUserHandler()
         {
             this.Command = "/empresanoregistrada";
-            this.State = UnregisteredCompanyUserState.Start;
-            this.Data = new UnregisteredCompanyUserData();
+            this.state = UnregisteredCompanyUserState.Start;
+            this.data = new UnregisteredCompanyUserData();
         }
         /// <summary>
         /// Pregunta por el codigo de invitacion y delega la tarea de verificar si el token es valido 
@@ -29,39 +37,39 @@ namespace ClassLibrary
         /// <param name="response"></param>
         public override bool InternalHandle(IMessage input, out string response)
         {
-            if (this.State == UnregisteredCompanyUserState.Start && CanHandle(input))
+            if (this.state == UnregisteredCompanyUserState.Start && CanHandle(input))
             {
                 StringBuilder datos = new StringBuilder("Asi que eres usuario de una empresa!\n")
                                                 .Append("Para poder registrarte vamos a necesitar el codigo de invitacion\n")
                                                 .Append("Ingrese el codigo de invitacion\n");
-                this.State = UnregisteredCompanyUserState.Token;
+                this.state = UnregisteredCompanyUserState.Token;
                 response = datos.ToString();
                 return true;
             }
-            else if(this.State == UnregisteredCompanyUserState.NotFirstTime)
+            else if(this.state == UnregisteredCompanyUserState.Token)
             {
-                this.Data.Token = input.Text;
-                this.State = UnregisteredCompanyUserState.Token;
-                response = "";
-                return true;
-            }
-            else if(this.State == UnregisteredCompanyUserState.Token)
-            {
-                this.Data.Token = input.Text;
-                if (TokenRegister.Instance.IsValid(this.Data.Token))
+                this.data.Token = input.Text;
+                if (TokenRegister.Instance.IsValid(this.data.Token))
                 {
-                    Company temp = TokenRegister.Instance.GetCompany(this.Data.Token);
+                    Company temp = TokenRegister.Instance.GetCompany(this.data.Token);
                     temp.AddUser(input.Id);
-                    this.State = UnregisteredCompanyUserState.Start;
+                    this.state = UnregisteredCompanyUserState.Start;
                     response = $"Se verifico el Token ingresado y se esta creando su usario perteneciente a la empresa {temp.Name}";
                     return true;
                 }
                 else
                 {
-                    this.State = UnregisteredCompanyUserState.NotFirstTime;
+                    this.state = UnregisteredCompanyUserState.NotFirstTime;
                     response = "No se pudo verificar el Token ingresado, ingrese nuevamente el token de invitacion";
                     return true;
                 }
+            }
+            else if(this.state == UnregisteredCompanyUserState.NotFirstTime)
+            {
+                this.data.Token = input.Text;
+                this.state = UnregisteredCompanyUserState.Token;
+                response = "";
+                return true;
             }
             else
             {
@@ -69,14 +77,31 @@ namespace ClassLibrary
                 return false;
             }
         }
-
+        
+        /// <summary>
+        /// Indica los diferentes estados que tiene UnregisteredCompanyUserHandler.
+        /// </summary>
         public enum UnregisteredCompanyUserState
         {
+            /// <summary>
+            /// El estado inicial del comando. Aquí pregunta por el token de invitacion necesario para 
+            /// registrar a un usuario empresa.
+            /// </summary>
             Start,
+            /// <summary>
+            /// Estado que vuelve a recibir un token en caso de que el ingresado anteriormente tenga
+            /// errores o no se encuentre en el registro.
+            /// </summary>
             NotFirstTime,
+            /// <summary>
+            /// Estado en el cual se verifica si el token ingresado es valido o no y se informa al usuario.
+            /// </summary>
             Token
         }
 
+        /// <summary>
+        /// Representa los datos que va obteniendo el comando UnregistredCompanyUserHandler en los diferentes estados.
+        /// </summary>
         public class UnregisteredCompanyUserData
         {
             /// <summary>
--- src/Library/Handlers/UnregistredUserHandlers/UnregisteredEntrepeneurUserHandler.cs
+++ src/Library/Handlers/UnregistredUserHandlers/UnregisteredEntrepeneurUserHandler.cs
@@ -10,9 +10,9 @@ namespace ClassLibrary
     /// </summary>
     public class UnregisteredEntrepeneurUserHandler : AbstractHandler
     {
-        public UnregisteredEntrepeneurUserState State { get; set; }
+        private UnregisteredEntrepeneurUserState state { get; set; }
 
-        public UnregisteredEntrepeneurUserData Data{ get; set; }
+        private UnregisteredEntrepeneurUserData data{ get; set; }
 
         /// <summary>
         /// Constructor de objetos UnregistredEntrepreneurUserHandler.
@@ -20,8 +20,8 @@ namespace ClassLibrary
         public UnregisteredEntrepeneurUserHandler()
         {
             this.Command = "/emprendedornoregistrado";
-            this.State = UnregisteredEntrepeneurUserState.Start;
-            this.Data = new UnregisteredEntrepeneurUserData();
+            this.state = UnregisteredEntrepeneurUserState.Start;
+            this.data = new UnregisteredEntrepeneurUserData();
         }
         /// <summary>
         /// Pregunta por los datos del emprendedor y delega la tarea de crear un usuario emprendedor.
@@ -30,56 +30,56 @@ namespace ClassLibrary
         /// <param name="response"></param>
         public override bool InternalHandle(IMessage input, out string response)
         {
-            if ((this.State == UnregisteredEntrepeneurUserState.Start) && (CanHandle(input)))
+            if ((this.state == UnregisteredEntrepeneurUserState.Start) && (CanHandle(input)))
             {
                 StringBuilder datos = new StringBuilder("Asi que eres un Emprendedor!\n")
                                                 .Append("Para poder registrarte vamos a necesitar algunos datos personales\n")
                                                 .Append("Ingrese su nombre completo\n");
-                this.State = UnregisteredEntrepeneurUserState.Name;
+                this.state = UnregisteredEntrepeneurUserState.Name;
                 response = datos.ToString();
                 return true;
             }
-            else if(this.State == UnregisteredEntrepeneurUserState.Name)
+            else if(this.state == UnregisteredEntrepeneurUserState.Name)
             {
-                this.Data.Name =  input.Text;
-                this.State = UnregisteredEntrepeneurUserState.Address;
+                this.data.Name =  input.Text;
+                this.state = UnregisteredEntrepeneurUserState.Address;
                 response = "Ingrese su dirección:\n";
                 return true;
             }
-            else if (this.State == UnregisteredEntrepeneurUserState.Address)
+            else if (this.state == UnregisteredEntrepeneurUserState.Address)
             {
-                this.Data.Address =  input.Text;
-                this.State = UnregisteredEntrepeneurUserState.City;
+                this.data.Address =  input.Text;
+                this.state = UnregisteredEntrepeneurUserState.City;
                 response = "Ingrese la ciudad:\n";
                 return true;
             }
-            else if(this.State == UnregisteredEntrepeneurUserState.City)
+            else if(this.state == UnregisteredEntrepeneurUserState.City)
             {
-                this.Data.City = input.Text;
-                this.State = UnregisteredEntrepeneurUserState.Department;
+                this.data.City = input.Text;
+                this.state = UnregisteredEntrepeneurUserState.Department;
                 response = "Ingrese el departamento:\n";
                 return true;
             }
-            else if (this.State == UnregisteredEntrepeneurUserState.Department)
+            else if (this.state == UnregisteredEntrepeneurUserState.Department)
             {
-                this.Data.Department = input.Text;
-                this.State = UnregisteredEntrepeneurUserState.Habilitations;
-                this.Data.LocationResult = new LocationAdapter(this.Data.Address,this.Data.City,this.Data.Department);
-                response = "Ingrese sus habilitaciones\n";
+                this.data.Department = input.Text;
+                this.state = UnregisteredEntrepeneurUserState.Habilitations;
+                this.data.LocationResult = new LocationAdapter(this.data.Address,this.data.City,this.data.Department);
+                response = "Ingrese el link a sus habilitaciones\n";
                 return true;
             }
-            else if (this.State == UnregisteredEntrepeneurUserState.Habilitations)
+            else if (this.state == UnregisteredEntrepeneurUserState.Habilitations)
             {
                 string habilitaciones =  input.Text;
-                this.State = UnregisteredEntrepeneurUserState.Headings;
+                this.state = UnregisteredEntrepeneurUserState.Headings;
                 response = "Ingrese su rubro\n";
                 return true;
             }
-            else if (this.State == UnregisteredEntrepeneurUserState.Headings)
+            else if (this.state == UnregisteredEntrepeneurUserState.Headings)
             {
                 string rubro = input.Text;
-                this.State = UnregisteredEntrepeneurUserState.Start;
-                UserRegister.Instance.CreateEntrepreneurUser(input.Id, this.Data.Name, this.Data.LocationResult, this.Data.Headings,this.Data.Habilitations);
+                this.state = UnregisteredEntrepeneurUserState.Start;
+                UserRegister.Instance.CreateEntrepreneurUser(input.Id, this.data.Name, this.data.LocationResult, this.data.Headings,this.data.Habilitations);
                 response = "Gracias por sus datos, se esta creando su usuario\n";
                 return true;
 
@@ -96,12 +96,34 @@ namespace ClassLibrary
         /// </summary>
         public enum UnregisteredEntrepeneurUserState
         {
+            /// <summary>
+            /// El estado inicial del comando. Aquí pregunta por el token de invitacion necesario para 
+            /// registrar a un usuario empresa.
+            /// </summary>
             Start,
+            /// <summary>
+            /// Estado en donde se guarda el nombre que envio el usuario y se pregunta por la direccion.
+            /// </summary>
             Name,
+            /// <summary>
+            /// Estado en donde se guarda la direccion que envio el usuario y se pregunta por la ciudad.
+            /// </summary>
             Address,
+            /// <summary>
+            /// Estado en donde se guarda la ciudad que envio el usuario y se pregunta por el departamento.
+            /// </summary>
             City,
+            /// <summary>
+            /// Estado en donde se guarda el departamento que envio el usuario, se encuentra la ubicacion y se pregunta por las habilitaciones.
+            /// </summary>
             Department,
+            /// <summary>
+            /// Estado en donde se guarda el link a las habilitaciones que envio el usuario y se pregunta por el rubro.
+            /// </summary>
             Habilitations,
+            /// <summary>
+            /// Estado en donde se guarda el rubro que envio el usuario, se crea el usuario emprendedor y se le informa al usuario.
+            /// </summary>
             Headings,
         }
 
